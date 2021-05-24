78f8b13f8649ecc60c34bbc6523e95659afeb8cb
NB1723 <notalia03@gmail.com>
2021-05-24 07:20:04 +0300

    lab6.4

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[5] = { 1, 1, 1, 1, 1 };
+			int b[5] = { 1, 1, 2, 1, 1 };
+			Assert::AreEqual(Ordered(a, a + 5), true);
+			Assert::AreEqual(Ordered(b, b + 5), false);
+		}
+	};
+}
